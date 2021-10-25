ffef4550431d641f7094fdb957a0050a90d4fbb6
王松松 <wss@wangsongsongs-MacBook-Pro.local>
2021-10-25 16:31:25 +0800

    add stm

--- web-learning/javascript/base/2_objects.js
+++ web-learning/javascript/base/2_objects.js
@@ -28,3 +28,61 @@ delete xiaoming.height;
 //or use the hasOwnProperty() to exclude the inherits;
 xiaoming.hasOwnProperty('name'); //true;
 xiaoming.hasOwnProperty('toString');//false; 
+
+/**
+ * 创建对象
+ */
+//工厂函数模式
+function createPerson(name, age) {
+    let o = new Object();
+    o.name = name;
+    o.age = age;
+    o.sayName = function () {
+        console.log(o.name);
+    }
+    return o;
+}
+let person0 = createPerson('0', 19);
+person0.sayName();
+//工厂模式缺点，没有类型标识,类型是Object
+
+//构造函数模式
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.sayName = function () {
+        console.log(this.name);
+    }
+}
+let personA = new Person('a', 12);
+personA.sayName();
+console.log(personA.constructor == Person);//true
+
+
+//构造函数2
+let Person = function (name, age) {
+    this.name = name;
+    this.age = age;
+    this.sayName = function () {
+        console.log(this.name);
+    }
+}
+let personVar1 = new Person('Var1', 18);
+
+//prototype parttern
+function Person() {
+    //共享变量
+    Person.prototype.name = 'nick';
+    Person.prototype.job = 'engineer';
+
+    //共享函数
+    Person.prototype.sayName = function () {
+        console.log(this.name);
+    }
+}
+let person1 = new Person;
+let person2 = new Person;
+person1.sayName();
+person2.sayName();
+
+console.log(person1.sayName === person2.sayName);
